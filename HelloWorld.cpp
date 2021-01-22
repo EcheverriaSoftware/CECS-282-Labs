c0bb2f510e08fdc619652b6a68543034eb287c16
EcheverriaSoftware <55721485+EcheverriaSoftware@users.noreply.github.com>
2021-01-22 12:44:22 -0800

    Create HelloWorld.cpp

---
+++ HelloWorld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std; 
+
+int main() {
+  string greeting = "Hello World";
+  cout << greeting << endl;
+  cout << "I Love C++!\n";
+  return 0;
+}
