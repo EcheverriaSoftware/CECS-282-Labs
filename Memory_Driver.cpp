2a367f9e4d09c26455022dd70738cadb90236d37
EcheverriaSoftware <55721485+EcheverriaSoftware@users.noreply.github.com>
2021-02-26 21:16:39 -0800

    Create Memory_Driver.cpp

---
+++ Memory_Driver.cpp
@@ -0,0 +1,19 @@
+// Melissa Gaines
+// Miguel Echeverria
+// Giovanni Contreras
+// Julian Saldana
+// Week 6 Lab 1
+// Time demoed: PM
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  long value1 = 200000, value2;
+  long *longPtr = &value1;
+  cout << *longPtr << endl;
+  value2 = *longPtr;
+  cout << value2 << endl;
+  cout << &value1 << endl;
+  cout << longPtr << endl;
+}
